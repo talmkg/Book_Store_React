7dbce6c61268df0b03d7e6cdf17030d0b9a1085e
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-11-23 18:03:45 +0100

    more improvements :D

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Button } from "react-bootstrap";
 function navbar() {
   return (
     <>
       <br />
       <Navbar bg="dark">
-        <Container>
-          <Navbar.Brand href="#home" className="d-flex align-items-center">
+        <Container className="d-flex">
+          <Navbar.Brand href="#home" className="d-flex">
             <h2 className="text-warning">锦筠</h2>
 
             <h2 className="text-light font-weight-bold">
               &nbsp;YummyBooks.net
             </h2>
           </Navbar.Brand>
+
+          <div>
+            <Button variant="info ">Profile</Button>
+          </div>
         </Container>
       </Navbar>
       <br />
